b99492595dbecc60a7bd1d2afeef36abbc0cba22
Adi Strete <adi4joy@yahoo.com>
2020-03-02 20:04:54 +0200

    Curs 13 Homework Explained

---
+++ curs13/curs13-code/src/ro/fasttrackit/curs13/homeworkexplained/Days.java
@@ -0,0 +1,11 @@
+package ro.fasttrackit.curs13.homeworkexplained;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ curs13/curs13-code/src/ro/fasttrackit/curs13/homeworkexplained/NoActivityForDayException.java
@@ -0,0 +1,7 @@
+package ro.fasttrackit.curs13.homeworkexplained;
+
+public class NoActivityForDayException extends Exception {
+    public NoActivityForDayException(String message) {
+        super(message);
+    }
+}
