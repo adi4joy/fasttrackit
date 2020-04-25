476b3bce06751f6b48eed8d2bfe17c833a466ec0
Adi Strete <adi4joy@yahoo.com>
2020-04-25 20:22:28 +0300

    Curs 20

--- curs20/curs20-code/src/test/java/ro/fasttrackit/curs20/country/CountryServiceTest.java
+++ curs20/curs20-code/src/test/java/ro/fasttrackit/curs20/country/CountryServiceTest.java
@@ -6,20 +6,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 class CountryServiceTest {
     CountryService service;
 
     @BeforeEach
     public void setup() {
         service = new CountryService(List.of(
-                new Country("Romania", "Bucharest", 19_000_000, 250_000, "Europe", List.of()),
-                new Country("Moldova", "Bucharest", 19_000_000, 250_000, "Europe", List.of())
+                new Country("Romania", "Bucharest", 19_000_000, 250_000, "Europe", List.of("BGR", "HUN", "MDA", "SRB", "UKR")),
+                new Country("Moldova", "Chisinau", 5_000_000, 250_000, "Europe", List.of())
         ));
     }
 
     @Test
-    @DisplayName("The countries names are displayed correctly")
+    @DisplayName("the contry names are displayed correctly")
     void countryNames() {
+        List<String> result = service.getCountriesName();
 
+        assertThat(result)
+                .containsExactly("Romania", "Moldova");
     }
 }
